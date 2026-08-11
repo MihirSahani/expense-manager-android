bf34e4b92e8ed9d36416f8339dfa6451fabc6d50
Mihir <sahanimihir@proton.me>
2026-08-11 06:21:09 +0530

    refactor(common): rework LazyListOfItems

--- core/common/src/main/java/com/example/common/ui/component/LazyListOfItems.kt
+++ core/common/src/main/java/com/example/common/ui/component/LazyListOfItems.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,36 +20,71 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.common.ui.theme.FinancesTheme
+import com.example.common.utils.MyText
 
 @Composable
-fun <T> LazyListOfItems(items: List<T>, content: @Composable (T) -> Unit) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(16.dp))
-            .background(MaterialTheme.colorScheme.surface)
-            .padding(vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+fun <T> LazyListOfItems(items: List<T>, title: String? = null, content: @Composable (T) -> Unit) {
+    Column(
+        Modifier.fillMaxWidth()
     ) {
-        items(items) { item ->
-            val isLast = items.indexOf(item) == items.size - 1
+        title?.let {
             Row(
-                Modifier
-                    .fillMaxWidth()
-                    // .padding(horizontal = 16.dp)
-                    .heightIn(min = 40.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                content(item)
+                MyText.RowBody(it)
+                HorizontalDivider(Modifier.padding(horizontal = 8.dp))
             }
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(bottom = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(items) { item ->
+                val isLast = items.indexOf(item) == items.size - 1
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        // .padding(horizontal = 16.dp)
+                        .heightIn(min = 40.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    content(item)
+                }
 
-            if (!isLast) {
-                HorizontalDivider(
-                    Modifier.padding(horizontal = 16.dp),
-                    thickness = 1.dp,
-                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-                )
+                if (!isLast) {
+                    HorizontalDivider(
+                        Modifier.padding(horizontal = 16.dp),
+                        thickness = 1.dp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LazyListOfItemsPreview() {
+    FinancesTheme {
+        ScreenScaffold("") { paddingValues ->
+            Column(
+                Modifier.fillMaxWidth().padding(top = paddingValues.calculateTopPadding()).padding(horizontal = 16.dp)
+            ) {
+                LazyListOfItems(items = listOf("Item 1", "Item 2", "Item 3"), "Lazy List Title") { item ->
+                    Text(
+                        text = item,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                    )
+                }
             }
         }
     }
@@ -56,11 +92,19 @@ fun <T> LazyListOfItems(items: List<T>, content: @Composable (T) -> Unit) {
 
 @Preview(showBackground = true)
 @Composable
-fun LazyListOfItemsPreview() {
-    LazyListOfItems(items = listOf("Item 1", "Item 2", "Item 3")) { item ->
-        Text(
-            text = item,
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
+fun LazyListOfItemsPreviewDark() {
+    FinancesTheme(true) {
+        ScreenScaffold("") { paddingValues ->
+            Column(
+                Modifier.fillMaxWidth().padding(top = paddingValues.calculateTopPadding()).padding(horizontal = 16.dp)
+            ) {
+                LazyListOfItems(items = listOf("Item 1", "Item 2", "Item 3"), "Lazy List Title") { item ->
+                    Text(
+                        text = item,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                    )
+                }
+            }
+        }
     }
 }
