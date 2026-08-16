527f18f22c54b961638bc180fbbf8b9de564557f
Mihir <sahanimihir@proton.me>
2026-08-16 18:39:48 +0530

    feat(home): add feature:home module with home screen and view model

--- feature/home/src/main/java/com/example/home/HomeScreen.kt
+++ feature/home/src/main/java/com/example/home/HomeScreen.kt
@@ -1,2 +1,35 @@
 package com.example.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.common.ui.component.ScreenScaffold
+import com.example.common.ui.theme.FinancesTheme
+
+@Composable
+fun HomeScreen(vm: HomeViewModel = hiltViewModel()) {
+    HomeContent()
+}
+
+@Composable
+fun HomeContent() {
+    ScreenScaffold("Home") { paddingValues ->
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeContentPreview() {
+    FinancesTheme {
+        HomeContent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeContentPreviewDark() {
+    FinancesTheme(true) {
+        HomeContent()
+    }
+}
